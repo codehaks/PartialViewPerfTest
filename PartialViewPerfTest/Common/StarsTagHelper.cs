4aef94025f8ee0e6cb09fa5a856103a6fa0927fc
Hakim Ghods <ghods.hakim@hotmail.com>
2018-11-01 01:42:59 +0330

    Add project files.

---
+++ PartialViewPerfTest/Common/StarsTagHelper.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Codehaks.TagHelpers
+{
+    public class StarsTagHelper : TagHelper
+    {
+        public int Level { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                if (i < Level)
+                {
+
+                    output.Content.AppendHtml("<i class='fa fa-star' style='color:#ffd800'></i>");
+                }
+                else
+                {
+                    output.Content.AppendHtml("<i class='fa fa-star-o'></i>");
+                }
+            }
+        }
+    }
+}
